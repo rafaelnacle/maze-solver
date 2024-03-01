ae9179b65bea3ee1a8ee930a0e9db8a5a10b56fb
Rafael Nacle <rafaelnacle@hotmail.com>
2024-03-01 17:59:04 -0300

    created Window class and initialized the constructor and Canvas

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import Tk, BOTH, Canvas
+
+
+class Window:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.root = Tk()
+        self.title = "Maze"
+        self.root.title(self.title)
+        self.canvas = Canvas(self.root, width=width, height=height)
+        self.canvas.pack(fill=BOTH, expand=1)
+        self.is_running = False
