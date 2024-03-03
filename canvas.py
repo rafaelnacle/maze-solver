63cfcb0f3637ae620f4c913e556a9ae7acbb0aac
Rafael Nacle <rafaelnacle@hotmail.com>
2024-03-03 12:04:36 -0300

    Window: added draw_line
    Added class Point
    Added class Line with the draw method

--- canvas.py
+++ canvas.py
@@ -21,5 +21,26 @@ class Window:
             self.redraw()
         print("window closed")
 
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
     def close(self):
         self.__running = False
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(
+            self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2
+        )
+        canvas.pack(fill=BOTH, expand=1)
