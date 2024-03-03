b08c0ce9f583246925772a7617980e777a149465
Rafael Nacle <rafaelnacle@hotmail.com>
2024-03-03 12:03:43 -0300

    added Line, Point and drawed a simple line

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from canvas import Window
+from canvas import Window, Line, Point
 
 
 def main():
-    win = Window(800, 600)
-    win.wait_for_close()
+    window = Window(800, 600)
+    line = Line(Point(30, 30), Point(300, 300))
+    window.draw_line(line, "black")
+    window.wait_for_close()
 
 
 main()
