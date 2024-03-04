20a355f13750c9960ca4547d8be47ee934453ac2
Rafael Nacle <rafaelnacle@hotmail.com>
2024-03-04 18:24:35 -0300

    created some cells for testing

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from canvas import Window, Line, Point
+from cell import Cell
 
 
 def main():
     window = Window(800, 600)
-    line = Line(Point(30, 30), Point(300, 300))
-    window.draw_line(line, "black")
+
+    cell = Cell(window)
+    cell.has_left_wall = False
+    cell.draw(30, 30, 100, 100)
+
+    cell = Cell(window)
+    cell.has_right_wall = False
+    cell.draw(135, 135, 200, 200)
+
+    cell = Cell(window)
+    cell.has_bottom_wall = False
+    cell.draw(100, 100, 250, 250)
+
+    cell = Cell(window)
+    cell.has_top_wall = False
+    cell.draw(440, 440, 500, 500)
+
     window.wait_for_close()
 
 
