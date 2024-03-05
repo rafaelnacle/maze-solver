c859b4e069948735a26f8c45fe70ebbde34df7d7
Rafael Nacle <rafaelnacle@hotmail.com>
2024-03-05 17:42:20 -0300

    Created the main layout for the maze

--- main.py
+++ main.py
@@ -1,33 +1,19 @@
-from canvas import Window, Line, Point
-from cell import Cell
+from canvas import Window
+from maze import Maze
 
 
 def main():
-    window = Window(800, 600)
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    window = Window(screen_x, screen_y)
 
-    cell1 = Cell(window)
-    cell1.has_right_wall = False
-    cell1.draw(50, 50, 100, 100)
-
-    cell2 = Cell(window)
-    cell2.has_left_wall = False
-    cell2.has_bottom_wall = False
-    cell2.draw(100, 50, 150, 100)
-
-    cell1.draw_move(cell2)
-
-    cell3 = Cell(window)
-    cell3.has_top_wall = False
-    cell3.has_right_wall = False
-    cell3.draw(100, 100, 150, 150)
-
-    cell2.draw_move(cell3)
-
-    cell4 = Cell(window)
-    cell4.has_left_wall = False
-    cell4.draw(150, 100, 200, 150)
-
-    cell3.draw_move(cell4, True)
+    maze = Maze(margin, margin, num_rows, num_cols,
+                cell_size_x, cell_size_y, window)
 
     window.wait_for_close()
 
