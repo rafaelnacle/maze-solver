edb0c2fa9547e35966e7c8c643f73861cb079e8c
Rafael Nacle <rafaelnacle@hotmail.com>
2024-03-07 17:28:25 -0300

    setted recursion limit and called the maze.solve()

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from canvas import Window
 from maze import Maze
+import sys
 
 
 def main():
@@ -10,10 +11,17 @@ def main():
     screen_y = 600
     cell_size_x = (screen_x - 2 * margin) / num_cols
     cell_size_y = (screen_y - 2 * margin) / num_rows
+    sys.setrecursionlimit(10000)
     window = Window(screen_x, screen_y)
 
     maze = Maze(margin, margin, num_rows, num_cols,
                 cell_size_x, cell_size_y, window, 10)
+    print("Maze created")
+    is_solvable = maze.solve()
+    if not is_solvable:
+        print("Maze cannot be solved")
+    else:
+        print("Maze solved!")
 
     window.wait_for_close()
 
