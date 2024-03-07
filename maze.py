d29513ef762548ae4f298ca5a3979323827f979a
Rafael Nacle <rafaelnacle@hotmail.com>
2024-03-07 16:48:01 -0300

    minor refactor on the names of the variables, created _break_entrance_and_exit method

--- maze.py
+++ maze.py
@@ -1,4 +1,5 @@
 from cell import Cell
+import random
 import time
 
 
@@ -11,7 +12,7 @@ class Maze:
         num_cols,
         cell_size_x,
         cell_size_y,
-        win=None,
+        window=None,
     ):
         self._cells = []
         self._x1 = x1
@@ -20,33 +21,40 @@ class Maze:
         self._num_cols = num_cols
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
-        self._win = win
+        self._window = window
 
         self._create_cells()
+        self._break_entrance_and_exit()
 
     def _create_cells(self):
         for i in range(self._num_cols):
             col_cells = []
             for j in range(self._num_rows):
-                col_cells.append(Cell(self._win))
+                col_cells.append(Cell(self._window))
             self._cells.append(col_cells)
         for i in range(self._num_cols):
             for j in range(self._num_rows):
                 self._draw_cell(i, j)
 
     def _draw_cell(self, i, j):
-        if self._win is None:
+        if self._window is None:
             return
         x1 = self._x1 + i * self._cell_size_x
         y1 = self._y1 + j * self._cell_size_y
         x2 = x1 + self._cell_size_x
         y2 = y1 + self._cell_size_y
         self._cells[i][j].draw(x1, y1, x2, y2)
-
         self._animate()
 
     def _animate(self):
-        if self._win is None:
+        if self._window is None:
             return
-        self._win.redraw()
+        self._window.redraw()
         time.sleep(0.05)
+
+    def _break_entrance_and_exit(self):
+        self._cells[0][0].has_top_wall = False
+        self._draw_cell(0, 0)
+        self._cells[self._num_cols - 1][self._num_rows -
+                                        1].has_bottom_wall = False
+        self._draw_cell(self._num_cols - 1, self._num_rows - 1)
